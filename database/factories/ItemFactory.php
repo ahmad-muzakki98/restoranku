a4daf25a3aa8d1c65d18bc042cb25bdfe70b1821
ahmad-muzakki98 <ahmadmuzakki8198@gmail.com>
2025-08-03 19:27:28 +0700

    Update ItemFactory to use specific image URLs and modify DatabaseSeeder to call ItemSeeder

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -14,7 +14,13 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 1000, 100000),
             'description' => $this->faker->text(),
-            'image' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement(
+                [
+                    'https://images.unsplash.com/photo-1591325418441-ff678baf78ef',
+                    'https://plus.unsplash.com/premium_photo-1668143358351-b20146dbcc02',
+                    'https://images.unsplash.com/photo-1738681335816-8e0df0aa9824'
+                ]
+            ),
             'is_active' => $this->faker->boolean(),
         ];
     }
